7947ce7ad659d1c44d5ab17ecbe1544192004fe3
haesoo-y <haesoo9410@gmail.com>
2024-12-21 22:12:17 +0900

    style: 색상 및 대표 이미지 수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -65,8 +65,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/social-card.jpg",
     navbar: {
       title: "Tech Logs",
       logo: {
