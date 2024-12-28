1c99a432da510205d295e3577f47557bbab4dd6a
haesoo-y <haesoo9410@gmail.com>
2024-12-28 10:11:40 +0900

    chore: 불필요한 파일 제거

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -130,7 +130,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Song Ku Hae Hyeon.`,
     },
     prism: {
       theme: prismThemes.github,
